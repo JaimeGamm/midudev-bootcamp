3af3eb666052891536a6b7040afca1c28cc4c726
jaimegamm <jaimegamm@gmail.com>
2024-01-15 23:30:39 -0500

    ejercic 1.1 del Bootcamp terminado

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  /**forma corta de Title
+  const Title = ({course})=><h1>{course}</h1>*/
+  const Title = (props)=> {
+    return <h1>{props.course}</h1>
+  }
+  const Parrafo = (props)=>{
+    return <p>
+    {props.part} {props.exercises}
+    </p>
+  }
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title course={course}/>
+      <Parrafo part={part1} exercises={exercises1}/>
+      <Parrafo part={part2} exercises={exercises2}/>
+      <Parrafo part={part3} exercises={exercises3}/>
+      <Parrafo part={"Number of exercises"} exercises={exercises1 + exercises2 + exercises3}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
